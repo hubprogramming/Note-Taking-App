cfd2ba1b43f297d0c58aede5d591fad2ca0dcc97
Mehnaz Arshad <hubprogramming4@gmail.com>
2024-09-04 15:02:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const notescontainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes(){
+    notescontainer.innerHTML = localStorage.getItem(".notes");
+}
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem("notes", notescontainer.innerHTML);
+}
+
+createBtn.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "images/delete.png";
+    notescontainer.appendChild(inputBox).appendChild(img);
+
+})
+
+notescontainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+    } 
+    else if(e.target.tagName === "p"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
